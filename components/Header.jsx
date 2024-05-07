0b7744a1fcea8d3df2362fbeb39ae560626f2b6a
GaziRiad <ouhibimustapha36@gmail.com>
2024-05-07 19:12:36 +0100

    Added logo to navigation

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,8 +3,8 @@ import Logo from "./Logo";
 
 function Header() {
   return (
-    <header className=" mx-auto mb-24 flex max-w-2xl items-center justify-center rounded-bl-full rounded-br-full bg-[#5ACBF0] py-3 lg:mb-40">
-      {/* <Logo /> */}
+    <header className=" relative mx-auto mb-24 flex max-w-2xl items-center justify-center rounded-bl-full rounded-br-full bg-[#5ACBF0] py-3 lg:mb-40">
+      <Logo />
       <ul className="flex items-center justify-center gap-6 font-semibold text-zinc-800">
         <li className="transition-all hover:text-primary">
           <Link href="/about">About Us</Link>
--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Logo() {
   return (
-    <Image
-      src="/images/messi.jpg"
-      height={500}
-      width={500}
-      alt="Marketing assistant website's logo"
-    />
+    <Link href="/">
+      <Image
+        src="/images/logo.png"
+        height={500}
+        width={500}
+        alt="Marketing assistant website's logo"
+        className="absolute bottom-0 left-0 w-12 translate-y-1/3 md:left-[10%]"
+      />
+    </Link>
   );
 }
 
