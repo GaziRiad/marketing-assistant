c0bdf1afd9163f9a460f02650db51170d807a8aa
GaziRiad <ouhibimustapha36@gmail.com>
2024-05-08 03:42:24 +0100

    Added favicon and fixed footer size in laptops

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import FooterCards from "./FooterCards";
 
 function Footer() {
   return (
-    <footer className="bg-secondary relative px-5 py-12">
+    <footer className="bg-secondary relative px-5 py-12 xl:px-52">
       <FooterCards />
       <Image
         width={500}
