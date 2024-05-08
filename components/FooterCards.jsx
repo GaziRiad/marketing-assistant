606afb93939ff324bb2d13acb09db092b0d46a9f
GaziRiad <ouhibimustapha36@gmail.com>
2024-05-08 02:23:18 +0100

    changed footer

--- components/FooterCards.jsx
+++ components/FooterCards.jsx
@@ -16,7 +16,7 @@ function FooterCards() {
             alt="Marketing assistant logo"
           />
         </div>
-        <Link
+        {/* <Link
           href="#"
           className="footer-card-shadow group relative col-start-1 row-span-3 flex w-full flex-col items-start justify-end rounded-2xl bg-primary px-6 py-6 pr-12 transition-all xl:gap-2 xl:py-12"
         >
@@ -101,91 +101,8 @@ function FooterCards() {
           <p className=" text-xl font-extrabold text-white xl:text-3xl">
             ENTERPRISE
           </p>
-        </Link>
+        </Link> */}
       </div>
-      {/* /////////////////////////////////////////////////////////////////////////////////////////////// */}
-      {/* /////////////////////////////////////////////////////////////////////////////////////////////// */}
-      {/* <div className="bg-background container mx-auto flex w-full flex-wrap gap-2 rounded-3xl p-3 xl:hidden">
-        <div className="group w-full rounded-2xl py-4">
-          <Image
-            src="/images/logotag.png"
-            height={300}
-            width={300}
-            alt="Marketing assistant logo"
-            className="mx-auto w-2/3"
-          />
-        </div>
-        <Link
-          href="#"
-          className="footer-card-shadow group relative flex w-full flex-col items-start justify-end gap-2 rounded-2xl bg-primary px-6 py-6 pr-12 transition-all"
-        >
-          <p className="text-xl tracking-tighter text-white">
-            <span>About</span>
-            <br />
-            <strong>US</strong>
-          </p>
-          <IoArrowForwardCircleOutline
-            size={48}
-            color="#fff"
-            className="-ml-2 transition-all group-hover:rotate-45"
-          />
-        </Link>
-        <Link
-          href="#"
-          className="footer-card-shadow group relative flex w-full items-end justify-start rounded-2xl bg-[#5ACBF0] px-6 py-6 pr-12 transition-all"
-        >
-          <IoIosAddCircle
-            color="#fff"
-            size={36}
-            className=" absolute right-4 top-4 transition-all group-hover:rotate-45"
-          />
-          <p className=" text-xl text-white">Design Templates</p>
-        </Link>
-        <Link
-          href="#"
-          className="footer-card-shadow group relative flex w-full items-end justify-start rounded-2xl bg-[#E2E2E2] px-6 py-6 pr-12 transition-all"
-        >
-          <IoIosAddCircle
-            color="#5ACBF0"
-            size={36}
-            className=" absolute right-4 top-4 transition-all group-hover:rotate-45"
-          />
-          <p className=" text-foreground text-xl">Copy Templates</p>
-        </Link>
-        <Link
-          href="#"
-          className="footer-card-shadow group relative flex w-full items-end justify-start rounded-2xl bg-[#E2E2E2] px-6 py-6 pr-12 transition-all"
-        >
-          <IoIosAddCircle
-            color="#FCDE67"
-            size={36}
-            className=" absolute right-4 top-4 transition-all group-hover:rotate-45"
-          />
-          <p className=" text-foreground text-xl">Guides & Tutorials</p>
-        </Link>
-        <Link
-          href="#"
-          className="footer-card-shadow group relative flex w-full items-end justify-start rounded-2xl bg-[#5ACBF0] px-6 py-6 pr-12 transition-all"
-        >
-          <IoIosAddCircle
-            color="#fff"
-            size={36}
-            className="absolute right-4 top-4 transition-all group-hover:rotate-45"
-          />
-          <p className=" text-xl text-white">Blog & News</p>
-        </Link>
-        <Link
-          href="#"
-          className="bg-secondary bg-footer footer-card-shadow group relative flex w-full items-end justify-start rounded-2xl bg-cover px-6 py-6 pr-12 transition-all"
-        >
-          <IoIosAddCircle
-            color="#fff"
-            size={36}
-            className=" absolute right-4 top-4 transition-all group-hover:rotate-45"
-          />
-          <p className=" text-xl font-extrabold text-white">ENTERPRISE</p>
-        </Link>
-      </div> */}
     </>
   );
 }
