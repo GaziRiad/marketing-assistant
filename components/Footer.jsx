312a5534608ee6400ae8852d4951b0dc9b4a76eb
GaziRiad <ouhibimustapha36@gmail.com>
2024-05-08 02:36:03 +0100

    changed footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,15 +3,15 @@ import FooterCards from "./FooterCards";
 
 function Footer() {
   return (
-    <footer className="bg-secondary relative min-h-full px-5 py-12">
+    <footer className="bg-secondary relative px-5 py-12">
       <FooterCards />
-      <Image
+      {/* <Image
         width={500}
         height={500}
         src="/ooorganize.svg"
         alt="svg"
         className="absolute -top-[10%] left-[0] -z-20 w-44"
-      />
+      /> */}
     </footer>
   );
 }
